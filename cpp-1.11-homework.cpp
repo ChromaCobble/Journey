d4b2e1c4ea8df970d91b4360ae3150f378c6eb7a
Ata Anıl Altıntaş <45094657+ChromaCobble@users.noreply.github.com>
2022-08-04 22:59:53 +0300

    Add files via upload

---
+++ cpp-1.11-homework.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num{ };
+
+	std::cout << "Please enter an integer :\n";
+
+	std::cin >> num;
+
+
+	std::cout << "Double the value of " << num << " is : " << num * 2 << '\n';
+
+	std::cout << "Triple the value of " << num << " is : " << num * 3 << '\n';
+
+	return 0;
+
+	
+
+}
---
+++ cpp-1.x_sum-homework.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num{ };
+	int num2{ };
+
+	std::cout << "Please enter an integer :\n";
+
+	std::cin >> num;
+
+	std::cout << "Please enter a second integer :\n";
+
+	std::cin >> num2;
+
+
+	std::cout << num << " + " << num2 << " is : " << num + num2 << '\n';
+
+	std::cout << num << " - " << num2 << " is : " << num - num2 << '\n';
+
+	return 0;	
+
+	
+
+}
